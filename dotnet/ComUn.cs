cf5af7852a719fea1113cfc16fb4534d728683cb
KB <kendallblack98@gmail.com>
2026-02-15 14:44:52 -0800

    clear cache

---
+++ dotnet/ComUn.cs
@@ -0,0 +1,8 @@
+﻿using System;
+//base combat unit class
+public class comUn
+{
+	public comUn()
+	{
+	}
+}
---
+++ dotnet/Engine.cs
@@ -0,0 +1,9 @@
+﻿using System;
+//todo: import csv reader, army class, comUn class, battle class
+public class LychEngine
+{
+	public LychEngine()
+	{
+		return;
+	}
+}
