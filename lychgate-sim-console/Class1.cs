5889807158bebf96d161d3a6d8cc27783690168d
KB <kendallblack98@gmail.com>
2026-02-15 12:41:53 -0800

    init C# project. created placeholder for class implementation updated gitignore to filter out stuff we don't need to track you can regenerate this gitignore for .NET by running: $ dotnet new gitignore

---
+++ lychgate-sim-console/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class LychEngine
+{
+	public LychEngine()
+	{
+	}
+}
---
+++ lychgate-sim-console/Engine.cs
@@ -0,0 +1,8 @@
+﻿using System;
+//todo: import csv reader, army class, comUn class, battle class
+public class LychEngine
+{
+	public LychEngine()
+	{
+	}
+}
