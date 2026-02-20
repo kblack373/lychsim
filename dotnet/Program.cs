894556e76bc9dc456e317defcf70f4b4648a7f76
KB <kendallblack98@gmail.com>
2026-02-19 18:19:03 -0800

    added user I/O to allow custom config files added shortbattle.json to demo 2 confif files
    added defaultconfig.json file to root of debug folder to allow quicker runs without retyping a target

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -3,6 +3,8 @@ namespace Lychgate
 {
     class Program
     {
+
+        private const string defaultFilePath = @".\defaultconfig.json";
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
@@ -10,10 +12,27 @@ namespace Lychgate
             //Console.ReadLine(); // keeps console open
 
             //LychEngine eng = new(@"C:\Users\kenda\OneDrive\Documents\development\lychgate\simulation\config\default.xml");
+            //= @"C:\Users\kenda\OneDrive\Documents\development\lychgate\simulation\config\default.json";
+
+            string filePath = "";
+            Console.WriteLine("LychGate Simulator v0.2");
+            Console.WriteLine("Enter absolute filepath to config.json (blank for default)> ");
+            filePath = Console.ReadLine().ToString();
+            if (filePath == "")
+            {
+
+                filePath = defaultFilePath;
+            }
+            if (System.IO.File.Exists(filePath))
+            {
+                LychEngine eng = new(filePath);
+                eng.RunSim();
+            } else
+            {
+
+                Console.WriteLine("Cannot read file.");
+            }
             
-            string filePath = @"C:\Users\kenda\OneDrive\Documents\development\lychgate\simulation\config\default.json";
-            LychEngine eng = new(filePath);
-            eng.RunSim();
         }
         //Console.ReadLine(); 
     }
